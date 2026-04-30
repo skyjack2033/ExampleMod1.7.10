4a3e52c58ba466cce4421c86f6d360b9ec4c4de6
Skyjack2033 <skyjack2033@163.com>
2026-04-30 12:01:28 +0800

    fix: exclude all shadowing stubs from JAR

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,25 @@ plugins {
 
 tasks.withType<Jar> {
     duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    // Exclude compatibility stubs that shadow real mod classes at runtime
+    exclude("codechicken/**")
+    exclude("appeng/me/**")
+    exclude("appeng/core/**")
+    exclude("appeng/client/**")
+    exclude("appeng/container/**")
+    exclude("appeng/helpers/**")
+    exclude("appeng/items/**")
+    exclude("appeng/parts/**")
+    exclude("appeng/tile/**")
+    exclude("appeng/util/**")
+    exclude("net/minecraft/block/properties/**")
+    exclude("net/minecraft/block/state/**")
+    exclude("net/minecraft/util/math/**")
+    exclude("net/minecraftforge/fluids/capability/**")
+    exclude("net/minecraftforge/items/**")
+    exclude("co/**")
+    exclude("com/circulation/**")
+    exclude("com/glodblock/**")
+    exclude("com/cleanroommc/**")
+    exclude("mezz/**")
 }
